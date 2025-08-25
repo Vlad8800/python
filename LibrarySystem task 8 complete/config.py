97b91781a0c5e0910908aeb809f2844912b1cb9a
Vlad8800 <93032225+Vlad8800@users.noreply.github.com>
2025-08-25 15:52:26 +0300

    Delete LibrarySystem task 8 complete directory

--- LibrarySystem task 8 complete/config.py
+++
@@ -1,6 +0,0 @@
-DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'Vlad8800',
-    'database': 'LibrarySystem'
-}
--- LibrarySystem task 8 complete/main.py
+++
@@ -1,4 +0,0 @@
-from login_window import show_login_window
-
-if __name__ == "__main__":
-    show_login_window()
